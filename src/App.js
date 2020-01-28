d17a50e55fa3f11b21c00badd1c0f3ca92dd1995
oussaka <kaabachi.dev@gmail.com>
2020-01-28 15:38:25 +0100

    create TodoList component for add new todo

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './App.css';
 import 'todomvc-app-css/index.css'
 
-function App() {
-    return (
-        <div className="todoapp">
-            <header className="header">
-                <h1>todos</h1>
-                <input
-                    className="new-todo"
-                    placeholder="What needs to be done?"
-                />
-            </header>
-        </div>
-    );
+class TodoList extends React.Component {
+    render () {
+        return (
+            <div className="todoapp">
+                <header className="header">
+                    <h1>todos</h1>
+                    <input
+                        className="new-todo"
+                        placeholder="What needs to be done?"
+                    />
+                </header>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default TodoList;
